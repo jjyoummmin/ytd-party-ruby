418c3d917e73ed05fa20fd2e1565d7ce77a354bb
jimin <dbwlals97@naver.com>
2021-06-13 15:16:35 +0900

    room 모델 생성, create_room 컨트롤러 생성

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,15 @@
 class ApiController < ApplicationController
     def create_room
+        VideoInfo.provider_api_keys = { youtube: ENV["YOUTUBE_API_KEY"] }
+        video = VideoInfo.new(params[:video_url])
+
         room = Room.new
         room.room_id = IdGenerator.generate
-        room.video_url = params[:video_url]
-        room.title = 
-        room.host = 
+        room.video_url = params[:video_url]        
+        room.title = video.title
+        room.host = "김아무개"       #=> devise 적용 후 수정할 것 current_user 
         room.save
         print params
-        redirect_to "/chat/#{IdGenerator.generate}"
+        redirect_to "/chat/#{room.room_id}"
     end
 end
