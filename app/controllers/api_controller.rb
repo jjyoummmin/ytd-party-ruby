e6cc4ff6a2865d2639ee8cd327ff56da188a560e
jimin <dbwlals97@naver.com>
2021-06-15 01:35:00 +0900

    api controller 수정

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,7 +7,8 @@ class ApiController < ApplicationController
         room.room_id = IdGenerator.generate
         room.video_id = params[:video_url].match(/(?<=\?v=).+$/).to_s        
         room.title = video.title
-        room.host = "김아무개"       #=> devise 적용 후 수정할 것 current_user 
+        room.host = current_user.name
+        room.host_email = current_user.email
         room.save
         print params
         redirect_to chat_path(:room_id => room.room_id, :video_id => room.video_id)
